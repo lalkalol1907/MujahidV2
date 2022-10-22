f245a078658e2429b5376c82947e74825cc71bc7
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2022-10-22 21:41:06 +0300

    refactoring

--- embeds.ts
+++ embeds.ts
@@ -1,13 +1,13 @@
 import Song from "./models/song";
 
-const { EmbedBuilder } = require('discord.js');
+const {EmbedBuilder} = require('discord.js');
 
 const timeFormat = (time: number): string => {
     let d = [((time - time % 3600) / 3600).toString(), ((time % 3600 - time % 3600 % 60) / 60).toString(), (time % 60).toString()]
     for (let i = 0; i < 3; i++) {
-        if (d[i].length === 1)
+        if (d[i].length === 1) {
             d[i] = "0" + d[i]
-
+        }
     }
     return `${d[0]}:${d[1]}:${d[2]}`
 }
@@ -30,10 +30,10 @@ const playingEmbed = (song: Song) => {
         .setColor(0x0099FF)
         .setTitle(song.title)
         .setURL(song.url)
-        .setAuthor({ name: 'Playing:', iconURL: song.message.author.avatarURL() })
+        .setAuthor({name: 'Playing:', iconURL: song.message.author.avatarURL()})
         .setThumbnail(song.pic)
         .addFields(
-            { name: 'Duration:', value: timeFormat(song.duration), inline: true },
+            {name: 'Duration:', value: timeFormat(song.duration), inline: true},
         )
 }
 
@@ -42,11 +42,11 @@ const addedToQueueEmbed = (song: Song, queueNumber: number) => {
         .setColor(0x0099FF)
         .setTitle(song.title)
         .setURL(song.url)
-        .setAuthor({ name: 'Added to queue:', iconURL: song.message.author.avatarURL() })
+        .setAuthor({name: 'Added to queue:', iconURL: song.message.author.avatarURL()})
         .setThumbnail(song.pic)
         .addFields(
-            { name: 'Duration:', value: timeFormat(song.duration), inline: true },
-            { name: 'Position in queue:', value: queueNumber.toString(), inline: true }
+            {name: 'Duration:', value: timeFormat(song.duration), inline: true},
+            {name: 'Position in queue:', value: queueNumber.toString(), inline: true}
         )
 }
 
@@ -56,11 +56,11 @@ const npEmbed = (song: Song, curTime: number = 0) => {
         .setDescription(formatLine(song.duration, (curTime - curTime % 1000) / 1000))
         .setTitle(song.title)
         .setURL(song.url)
-        .setAuthor({ name: 'Now playing:', iconURL: song.message.author.avatarURL() })
+        .setAuthor({name: 'Now playing:', iconURL: song.message.author.avatarURL()})
         .setThumbnail(song.pic)
         .addFields(
-            { name: 'Duration:', value: timeFormat(song.duration), inline: true },
-            { name: 'Current time', value: timeFormat((curTime - curTime % 1000) / 1000), inline: true }
+            {name: 'Duration:', value: timeFormat(song.duration), inline: true},
+            {name: 'Current time', value: timeFormat((curTime - curTime % 1000) / 1000), inline: true}
         )
 }
 
