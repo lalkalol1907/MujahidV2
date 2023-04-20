275caf13eded765f7cb679135bbbd759f3cc757e
Konstantin <konstantin.leonov1907@gmail.com>
2023-04-20 19:46:28 +0300

    Update decrypt_config.sh

--- decrypt_config.sh
+++ decrypt_config.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$CONFIG_PHRASE" \
---output $HOME/secrets/config.json config.json.gpg
+--output ./config.json config.json.gpg
